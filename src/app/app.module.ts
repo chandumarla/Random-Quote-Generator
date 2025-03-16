b0c95eb399e8f03c9764010d7dd05edccd12d41e
Chandu Marla <chandumarla99@gmail.com>
2025-03-16 12:38:45 +0530

    Initial commit - Random quote generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';  //---->  To use HttpClientModule
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { QuoteComponent } from './quote/quote.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,  //---> Added 
   ],
   providers: [],
   bootstrap: [AppComponent]
